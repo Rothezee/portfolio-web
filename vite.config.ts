2da0f3f6181ad01222c869457d170ab821c2566b
Rothezee <rothalan83@gmail.com>
2025-07-03 20:43:53 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  base: '/portfolio-web/', // Cambia esto por el nombre de tu repositorio
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
